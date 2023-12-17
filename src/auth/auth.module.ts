b79c0d22b1e2387f4b217118c2fff9627db1f02c
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-12-17 19:23:49 +0100

    login user

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
   imports: [UserModule],
 })
 export class AuthModule {}
