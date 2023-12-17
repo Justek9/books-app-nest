1aa123d3662106c1bc5f20081332645dd27a2c9b
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-12-17 12:30:01 +0100

    add user module, service and controller, update db

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthorsModule, BooksModule],
+  imports: [AuthorsModule, BooksModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
